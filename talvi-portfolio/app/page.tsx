58832fe88327c0f2a848156f48d91b6664e8a861
TalvenlapsiDot <kukkajessika@gmail.com>
2024-03-08 21:43:13 +0200

    Basic layout

--- talvi-portfolio/app/page.tsx
+++ talvi-portfolio/app/page.tsx
@@ -1,8 +1,77 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { Card, CardBody, Box, HStack, Icon, Spacer, Stat, StatLabel, StatHelpText, Flex, Wrap, WrapItem} from "@chakra-ui/react";
+import { DiHtml5, DiReact, DiCss3 } from "react-icons/di";
+import { TbBrandNextjs,} from "react-icons/tb";
+import { RiJavascriptLine } from "react-icons/ri";
+
 
 export default function Home() {
   return (
-    <h1> Hi </h1>
+  <>
+    <Box
+      p="5"
+      maxWidth="70%"
+      bg="blackAlpha.400"
+      borderRadius='20'>
+      <Wrap spacing='10px' align="left">
+        <Card
+          variant="elevated"
+          size="md"
+          bg="whiteAlpha.200"
+          width='300px'
+          height='80px'>
+            <CardBody textColor="whiteAlpha.800"> Alalalalalalala </CardBody>
+        </Card>
+        <Card
+          variant="elevated"
+          size="md"
+          bg="whiteAlpha.200"
+          width='100px'
+          height="80px">
+            <CardBody textColor="whiteAlpha.800"> Test </CardBody>
+        </Card>
+        <Card
+          variant="elevated"
+          size="md"
+          bg="whiteAlpha.200"
+          width='100px'
+          height="80px">
+            <CardBody textColor="whiteAlpha.800"> Test </CardBody>
+        </Card>
+          <Spacer/>
+        <Card
+          variant="elevated"
+          size="md"
+          maxW="60%"
+          height="100px"
+          bg="whiteAlpha.200">
+            <CardBody textColor="whiteAlpha.800">
+              <Flex direction="row">
+                <Stat marginRight="2">
+                  <StatLabel><Icon as={DiHtml5} boxSize="10"/></StatLabel>
+                  <StatHelpText> Html5</StatHelpText>
+                </Stat>
+                <Stat marginRight="2">
+                  <StatLabel><Icon as={DiCss3} boxSize="10"/></StatLabel>
+                  <StatHelpText>CSS</StatHelpText>
+                </Stat>
+                <Stat marginRight="2">
+                  <StatLabel><Icon as={RiJavascriptLine} boxSize="10"/></StatLabel>
+                  <StatHelpText>JavaScript</StatHelpText>
+                </Stat>
+                <Stat marginRight="2">
+                  <StatLabel><Icon as={TbBrandNextjs} boxSize="10"/></StatLabel>
+                  <StatHelpText>Next.JS</StatHelpText>
+                </Stat>
+                <Stat marginRight="2">
+                  <StatLabel><Icon as={DiReact} boxSize="10"/></StatLabel>
+                  <StatHelpText>React</StatHelpText>
+                </Stat>
+              </Flex>
+            </CardBody>
+        </Card>
+    </Wrap>
+  </Box>
+  </>
   );
 }
--- talvi-portfolio/app/providers.tsx
+++ talvi-portfolio/app/providers.tsx
@@ -1,8 +1,27 @@
 // app/providers.tsx
 'use client'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#B2349D",
+      100: "#7D2484",
+      200: "#B23474",
+      300: "#6C34BF",
+      400: "#4B2486",
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'transparent',
+      }
+    }
+  }
+})
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
 }
